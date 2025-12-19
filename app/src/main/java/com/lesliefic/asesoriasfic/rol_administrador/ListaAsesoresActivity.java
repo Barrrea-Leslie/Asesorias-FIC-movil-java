5acb67f143dcbc568c20e0f7b10d5d64a5b8381e
Jose290204 <joseangelastorga29@gmail.com>
2025-12-18 17:04:11 -0700

    se le dio funcionalidad a los botones de crear asesor de los asesores par y disciplinar, tambien se agrego el boton para regresar a la pantalla anterior en la pantalla de crear asesor

--- app/src/main/java/com/lesliefic/asesoriasfic/rol_administrador/ListaAsesoresActivity.java
+++ app/src/main/java/com/lesliefic/asesoriasfic/rol_administrador/ListaAsesoresActivity.java
@@ -3,6 +3,7 @@ package com.lesliefic.asesoriasfic.rol_administrador;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,8 +11,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.lesliefic.asesoriasfic.R;
 import com.lesliefic.asesoriasfic.adaptador.AsesorParAdapter;
 import com.lesliefic.asesoriasfic.databinding.ActivityAdminListaAsesoresBinding;
+import com.lesliefic.asesoriasfic.login.loginActivity;
 import com.lesliefic.asesoriasfic.modelo.AsesorPar;
 import com.lesliefic.asesoriasfic.modelo.AsesoresParRepository;
+import com.lesliefic.asesoriasfic.rol_administrador.crearAsesoresPar;
 
 import java.util.List;
 
@@ -41,6 +44,17 @@ public class ListaAsesoresActivity extends DrawerBaseActivity {
 
         btnCrearAsesorPar = findViewById(R.id.btnCrearAsesor);
 
+        btnCrearAsesorPar.setOnClickListener(v -> {
+
+            Intent intent = new Intent(ListaAsesoresActivity.this, crearAsesoresPar.class);
+            startActivity(intent);
+        });
+
 
     }
+
+    public void toastNotificacion(String message){
+        Toast.makeText(ListaAsesoresActivity.this, message, Toast.LENGTH_SHORT).show();
+    }
+
 }
